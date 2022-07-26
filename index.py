aa21eb80bf0c479be5d467ae0fe642dbb5c2eff2
Breno Soares <brenosoares4303@gmail.com>
2022-07-26 08:26:50 -0300

    Added account creation and login system with json file

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 #region Imports
+from operator import contains
 from geopy.geocoders import Nominatim
 import geocoder
 import json
@@ -21,8 +22,8 @@ def getLocation():
 # Function for get what type of user is (Customer or employeer)
 def getUser():
   user_type = input("Hello! Who are you? Type 1 for CUSTOMER or 2 for EMPLOYEER\n")
-  if user_type == "1": return "Customer"
-  elif user_type == "2": return "Employeer"
+  if user_type == "1": return "Customers"
+  elif user_type == "2": return "Employees"
   else:
     print("Invalid answer, try again!")
     getUser()
@@ -30,15 +31,50 @@ def getUser():
 def createSafePassword():
   return "123123"
 
-def ValidateCredentials(user_type, username, password):
-  return {
-    "validated": True,
-    "username": username,
-    "password": password
-  }
+def ValidateCredentials(user_type, username, password, caller):
+  file = open("./db/db.json", "r+")
+  db = json.load(file)
+  file.close()
+  users = list(db["users"][user_type.lower()].values())
+
+  have_account = []
+
+  if caller == "create":
+    for user in users:
+      if user["username"] == username:
+        have_account.append(True)
+      else:
+        have_account.append(False)
+
+    if True in have_account:
+      return {
+        "validated": False
+      }
+    else:
+      return {
+        "validated": True,
+        "user": {
+          "username": username,
+          "password": password
+        }
+      }
+  else:
+    for user in users:
+      if(user["username"] == username and user["password"] == password):
+        return {
+          "validated": True,
+          "user": {
+            "username": username,
+            "password": password
+          }
+        }
+      else:
+        return {
+          "validated": False
+        }
 #endregion
 
-# App class
+#region App Class
 class NerdFlix:
   
   #Federative Unit
@@ -50,31 +86,70 @@ class NerdFlix:
 
   #Create account if user haven't
   def CreateAccount(self):
-    username = input("What'll be your username?")
-    password = input("Type your password, use a secure one! If you want type 123 and we create a safe password to you!")
+    username = input("What'll be your username?\n")
+    password = input("Type your password, use a secure one! If you want type 123 and we create a safe password to you!\n")
     if password == "123":
       password = createSafePassword()
     else:
-      response = ValidateCredentials(self.user_type, username, password)
-      # if response["validated"] == True:
-      #   #proceed
+      response = ValidateCredentials(self.user_type, username, password, "create")
+      if response["validated"] == True:
+        
+        file = open("./db/db.json", "r+")
+        db = json.load(file)
+        file.close()
+
+        if len(list(db["users"][self.user_type.lower()].keys())) > 0:
+          id = format(int(list(db["users"][self.user_type.lower()].keys())[-1]) + 1, '04d')
+        else:
+          id = "0001"
+
+        user = {
+          "username": username,
+          "password": password
+        }
+
+        db["users"][self.user_type.lower()][id] = user
+        to_change_file = open("./db/db.json", "w")
+        json.dump(db, to_change_file, indent=2)
+        to_change_file.close()
+
+        print("Account created succesfully!")
+        self.active_user = response["user"]
+      else:
+        print("We saw here that you already have an account, try to login!")
+        nerdFlix.Login()
 
   
   #Login user with her credentials
   def Login(self):
     username = input("Alright! Give me you username\n")
     password = input("Received! Now your password, I promise it'll be very well protected\n")
+    response = ValidateCredentials(self.user_type, username, password, "login")
+    if response["validated"] == True:
+      print("Logged!")
+      self.active_user = response["user"]
+    else:
+      print("Invalid credentials, try again!")
+      self.Login()
 
-    self.active_user["username"] = username
-    self.active_user["password"] = password
+  def Dashboard(self):
+    if self.user_type == "Customers":
+      print(
+        f"Hello! {self.active_user['username']}, what do you want to do?\n"
+        "1 - Let's buy!\n"
+        "2 - Update my account\n"
+        "3 - Delete my account\n"
+        "4 - Exit NerdFlix"  
+      )
+#endregion
 
+#region Interactions
 #Instantiate App class
 nerdFlix = NerdFlix()
 
 #Set what type of user is using the app
 nerdFlix.user_type = getUser()
 
-#Get if the user have account and send her to Login or Create Account
 while True:
   have_account = input(f"Hello Mr/Ms {nerdFlix.user_type}, Do you have an account? Type 1 for YES or 2 for NO\n")
   if have_account == "1": 
@@ -86,4 +161,6 @@ while True:
   else:
     print("Invalid answer, try again!")
 
-print(nerdFlix.active_user)
+if nerdFlix.active_user:
+  nerdFlix.Dashboard()
+#endregion
